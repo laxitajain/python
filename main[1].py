7490a4141a0fc37b42b1b4d40eef89b14106ed05
laxita <87566518+laxitajain@users.noreply.github.com>
2023-12-26 13:58:10 +0530

    swapping elems from 2 lists

--- main[1].py
+++ main[1].py
@@ -1,11 +1,6 @@
-n=int(input("Enter the number of items: "))
-print("Enter the items: ")
-list1=[]
-for i in range(n):
-    item=int(input())
-    list1.append(item)
-if n%2==0:
-    list1[n//2-1:n//2+1]=[]
-else:
-    list1[n//2:n//2+1]=[]
-print(list1)
+list_a = [1, 2, 3]
+list_b=[4,5,6]
+list_a[0],list_b[2]= list_b[2],list_a[0]
+list_a[1],list_b[1]=list_b[1], list_a[1]
+print(list_a)
+print(list_b)
