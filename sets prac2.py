8c607c46eab179a69ae5729d119092692b5b6a43
laxita <87566518+laxitajain@users.noreply.github.com>
2024-01-04 14:06:39 +0530

    Add files via upload

---
+++ sets prac2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+#68
+set1={2,3,4,5}
+updateset={6,7,8}
+set1.update(updateset)
+print(f"The updated set is:{set1}")
+
+
+# In[4]:
+
+
+#69
+set1={3,4,5,6,7,8}
+print(f"The length of the set is: {len(set1)}")
+
+
+# In[5]:
+
+
+#70
+set1={2,3,4,5,6}
+del1=5
+set1.remove(del1)
+print(f"The updated set is: {set1}")
+
+
+# In[ ]:
+
+
+
+
