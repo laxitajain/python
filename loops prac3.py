ff139283a66addcf2bd91afd7a961532f627cdb6
laxita <87566518+laxitajain@users.noreply.github.com>
2024-01-10 08:09:15 +0530

    Update

---
+++ loops prac3.py
@@ -0,0 +1,60 @@
+#1
+n=int(input("Enter the number: "))
+for i in range(1,11):
+    print(f"{n}x{i}={n*i}")
+
+ #2
+n=int(input("Enter the number if classes held: "))
+a=int(input("Enter the number of classes attended: "))
+if (a/n)*100<75:
+    print("Not allowed!")
+else:
+    print("Allowed.")
+
+#3
+y=int(input("Enter years of service: "))
+if y<=5:
+    print("No bonus!")
+else:
+    s=int(input("Enter salary: "))
+    print("Bonus is",(5*s)/100)
+
+#5
+q=int(input("Enter Quantity: "))
+if q*100>1000:
+    print("Cost is",90*q)
+else:
+    print("Cost is",q*100)
+
+
+
+#1
+n=int(input("Enter the number: "))
+for i in range(1,11):
+    print(f"{n}x{i}={n*i}")
+
+ #2
+n=int(input("Enter the number if classes held: "))
+a=int(input("Enter the number of classes attended: "))
+if (a/n)*100<75:
+    print("Not allowed!")
+else:
+    print("Allowed.")
+
+#3
+y=int(input("Enter years of service: "))
+if y<=5:
+    print("No bonus!")
+else:
+    s=int(input("Enter salary: "))
+    print("Bonus is",(5*s)/100)
+
+#5
+q=int(input("Enter Quantity: "))
+if q*100>1000:
+    print("Cost is",90*q)
+else:
+    print("Cost is",q*100)
+
+
+
